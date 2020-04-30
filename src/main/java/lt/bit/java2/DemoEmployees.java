0a07ba314597e0816816643a39e9a02aab50677c
Valdas <valdas.ziemys@gmail.com>
2020-04-30 10:32:39 +0300

    mysql ir application.properties

--- src/main/java/lt/bit/java2/DemoEmployees.java
+++ src/main/java/lt/bit/java2/DemoEmployees.java
@@ -1,5 +1,14 @@
 package lt.bit.java2;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
 public class DemoEmployees {
 
     /*
@@ -12,8 +21,29 @@ public class DemoEmployees {
      */
     public static void main(String[] args) {
 
+        Properties properties = new Properties();
+        try {
+            // failas 'application.properties' ieskomas projekto katalogo virsuje,
+            // t.y. kataloge is kurio paleidziama programa
+            // InputStream is = new FileInputStream("application.properties");
 
+            InputStream is = DemoEmployees.class.getClassLoader().getResourceAsStream("application.properties");
+            properties.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
 
-
+        try (
+                Connection conn = DriverManager.getConnection(
+                    properties.getProperty("db.url"),
+                        properties.getProperty("db.user"),
+                        properties.getProperty("db.password"));
+                Statement statement = conn.createStatement();
+        ) {
+            /* you use the connection here */
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
