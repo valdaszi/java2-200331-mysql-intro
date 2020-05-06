44b19ca88eac24fff4cc2dc23d40c03a531de6fb
Valdas <valdas.ziemys@gmail.com>
2020-05-06 10:08:08 +0300

    Uzdavinukas su testais ir H2 DB - pabaigti testus ir EmployeeService.loadEmployees metoda

--- src/main/java/lt/bit/java2/services/EmployeeService.java
+++ src/main/java/lt/bit/java2/services/EmployeeService.java
@@ -2,6 +2,8 @@ package lt.bit.java2.services;
 
 import lt.bit.java2.model.Employee;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class EmployeeService {
@@ -13,7 +15,13 @@ public class EmployeeService {
      * @return
      */
     public static List<Employee> loadEmployees(int pageNo, int pageSize) {
-        // TODO - uzkrauti employee irasus kartu su salary is DB
+        try (Connection connection = DBService.getConnectionFromCP()) {
+            // TODO - uzkrauti employee irasus kartu su salary is DB
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
